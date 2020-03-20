c61fa3fe09d2f75b45a2cdc10ac89f6547d81749
kihiuFrank <franklinekihiu@gmail.com>
2020-03-20 22:19:40 +0300

    first commit

--- app/src/main/java/com/example/healthcloudstore/MainActivity.kt
+++ app/src/main/java/com/example/healthcloudstore/MainActivity.kt
@@ -2,21 +2,20 @@ package com.example.healthcloudstore
 
 import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import android.widget.Toast.LENGTH_LONG
+import androidx.appcompat.app.AppCompatActivity
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
-
+import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    lateinit var providers : List<AuthUI.IdpConfig>
+    lateinit var providers : List<AuthUI.IdpConfig>	
     private val RC_SIGN_IN = 123
 
 
@@ -29,16 +28,25 @@ class MainActivity : AppCompatActivity() {
             AuthUI.IdpConfig.EmailBuilder().build(),   //Email Login
             AuthUI.IdpConfig.GoogleBuilder().build())  //Google Login
 
-        showSignInOption()
 
-    }
-
-    private fun showSignInOption() {
         // Create and launch sign-in intent
-        startActivityForResult(AuthUI.getInstance()
+        startActivityForResult(
+            AuthUI.getInstance()
                 .createSignInIntentBuilder()
                 .setAvailableProviders(providers)
-                .build(), RC_SIGN_IN)
+                .build(),
+            RC_SIGN_IN)
+
+//        showSignInOption()
+//
+//    }
+//
+//    private fun showSignInOption() {
+//        // Create and launch sign-in intent
+//        startActivityForResult(AuthUI.getInstance()
+//                .createSignInIntentBuilder()
+//                .setAvailableProviders(providers)
+//                .build(), RC_SIGN_IN)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -50,13 +58,15 @@ class MainActivity : AppCompatActivity() {
             if (resultCode == RESULT_OK) {
                 // Successfully signed in
                 val user = FirebaseAuth.getInstance().currentUser //get current user
-                Toast.makeText(this, "" + user!!.email, Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Welcome" + user!!.email, Toast.LENGTH_SHORT).show()
             } else {
                 // Sign in failed, check response for error code
-                Toast.makeText(this, "" + response!!.error!!.message, Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Oops!! Error signing in" + response!!.error!!.message, Toast.LENGTH_SHORT).show()
             }
         }
     }
+
+
 
 
 
@@ -65,7 +75,12 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
+        menuInflater.inflate(R.menu.actions_menu, menu)
+
+        //OR
+/*        val inflater = menuInflater
+        inflater.inflate(R.menu.actions_menu, menu)*/
+
         return true
     }
 
@@ -74,10 +89,32 @@ class MainActivity : AppCompatActivity() {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when(item.itemId) {
-            R.id.action_settings -> true
+            R.id.log_out -> signOut()
+
+
+
+            
             else -> super.onOptionsItemSelected(item)
         }
     }
 
+    private fun signOut() : Boolean{
+
+        var status: Boolean = false
+        AuthUI.getInstance()
+            .signOut(this)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(this, getString(R.string.signed_out), LENGTH_LONG).show()
+                    status  =  true
+                }
+                else{
+                    Toast.makeText(this, "Sign out failed", LENGTH_LONG).show()
+                }
+
+            }
+
+        return status
+    }
 
 }
